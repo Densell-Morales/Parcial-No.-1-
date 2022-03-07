0a9d97a6fd4da9d97456f3e7228d2149acd9b6a0
Densell <dguevaram4@miumg.edu.gt>
2022-03-06 22:19:39 -0600

    Primer commit calcular factorial

---
+++ main.cpp
@@ -0,0 +1,45 @@
+using namespace std;
+#include "iostream"
+#include "string"
+#include<locale.h>
+// Densell Andres Guevara Morales 9090-20-11193
+
+using namespace std;
+
+int main()
+{
+
+float nm, nm1, nm2;
+int fact = 1;
+int opcion;
+
+cout << "Eslige Una de las acciones que quieres realizar: Calcular la factorial de un numero[1]  Tabla de multiplicar[2]  Calcular numeros pares[3]  Que dia de la semana es laboral[4]  Comprobar contraseña bancaria[5]";
+cin >> opcion;
+
+
+switch (opcion)
+{
+    case 1:
+        cout << "Elejiste Calcular la factorial de un numero" << endl;
+        printf ("\n");
+
+        printf("Ingresa el numero que deseas factorizar");
+        cin>>nm;
+
+        for(int i=1; i<=nm;i++){
+            fact= fact*= i;
+        }
+cout<< "El resultado del factorial es:"<<fact<<endl;
+
+
+
+
+
+        break;
+
+
+}
+
+
+    return 0;
+}
